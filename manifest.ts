236a894a7f97dfb0a325cab924a8e6e51f343897
RieN7 <zrien7@gmail.com>
2023-09-14 16:40:17 +0800

    🔧 build: update manifest

--- manifest.ts
+++ manifest.ts
@@ -2,11 +2,16 @@ import type { ManifestV3Export } from '@crxjs/vite-plugin'
 
 const manifest: ManifestV3Export = {
   manifest_version: 3,
-  name: 'My Extension',
+  name: 'Cooky',
   version: '1.0.0',
   permissions: ['storage'],
   action: {
     default_popup: 'src/popup/index.html',
+  },
+  icons: {
+    16: 'src/assets/icon-16.png',
+    48: 'src/assets/icon-48.png',
+    128: 'src/assets/icon-128.png',
   },
   content_scripts: [{
     matches: ['<all_urls>'],
